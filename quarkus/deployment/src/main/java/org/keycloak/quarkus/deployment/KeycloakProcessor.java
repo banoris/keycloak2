131bb4108c8ad0cb4c69328e1bbc3fbf7f6d12d2
Peter Zaoral <pzaoral@redhat.com>
2026-02-05 21:26:06 +0100

    [quarkus-next] Test fails due to missing build step ordering constraint on disableHealthCheckBean

--- quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
+++ quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
@@ -837,6 +837,7 @@ class KeycloakProcessor {
     }
 
     @BuildStep
+    @Consume(ProfileBuildItem.class)
     void disableHealthCheckBean(BuildProducer<BuildTimeConditionBuildItem> removeBeans, CombinedIndexBuildItem index) {
         if (isHealthDisabled()) {
             disableReadyHealthCheck(removeBeans, index);
